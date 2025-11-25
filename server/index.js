55e82629ffc91356d1074c91e4f61e3d0d627fd0
arcjun01 <arciaga.juno@student.greenriver.edu>
2025-11-25 13:58:51 -0800

    Updated index.js and installed cors.

--- server/index.js
+++ server/index.js
@@ -2,8 +2,10 @@ import 'dotenv/config';
 import express from "express";
 import jobsRouter from "./routes/jobs.js";
 import videosRouter from "./routes/videos.js";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 // Static videos
